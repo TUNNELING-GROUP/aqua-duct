5e4646b2351bc19d94258844651612fd0b3bbc24
Tomasz Magdziarz <tljm@Tom>
2015-11-30 10:54:10 +0100

    adding tests and test data

--- src/aquarium/traj/reader.py
+++ src/aquarium/traj/reader.py
@@ -6,10 +6,24 @@ Created on Nov 19, 2015
 
 import MDAnalysis as mda
 
-class ReadAmberNC(object):
+class Reader(object):
+    
     def __init__(self,topology,trajectory):
-        assert isinstance(topology, string)
-        assert isinstance(trajectory, string)
+        assert isinstance(topology, str)
+        assert isinstance(trajectory, str)
         
         self.topology_file_name = topology
         self.trajectory_file_name = trajectory
+    
+    def open_trajectory(self):
+        raise NotImplementedError()
+    
+        
+class ReadAmberNetCDF(Reader):
+    pass
+
+
+
+
+reader = ReadAmberNetCDF("topology","trajectory")
+reader.open_trajectory()
