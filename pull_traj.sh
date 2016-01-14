5df4cf1d6318a4498cffeb9164b63c1034dc1e73
Tomasz Magdziarz <tomasz.magdziarz@polsl.pl>
2016-01-14 14:08:27 +0100

    better pull

--- pull_traj.sh
+++ pull_traj.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -avz -P --delete tljm@192.168.1.15:/home/tljm/Work/aqua/real_traj/ ./real_traj/
+rsync -avzC -P --delete tljm@192.168.1.15:/home/tljm/Work/aqua/real_traj/ ./real_traj/
 
