47266cfac0797d62ee45d3ed9a21fb4f66c40ee3
Tomasz Magdziarz <tljm@Tom>
2016-01-15 16:47:37 +0100

    minor improvements in pull traj data script python requirements list

--- pull_traj.sh
+++ pull_traj.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-rsync -avzC -P --delete tljm@192.168.1.15:/home/tljm/Work/aqua/real_traj/ ./real_traj/
+rsync -av -P --delete tljm@192.168.1.15:/home/tljm/Work/aqua/real_traj/ ./real_traj/
 
