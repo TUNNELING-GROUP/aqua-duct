5423be9c6f1a884254ba4a2924ff7e7197a5e6a6
Tomasz Magdziarz <t.magdziarz@tunnelinggroup.pl>
2016-11-22 13:57:47 +0100

    License information added to valve

--- src/apps/valve.py
+++ src/apps/valve.py
@@ -1,6 +1,23 @@
 #!/bin/env python2
 # -*- coding: utf-8 -*-
 
+# Aqua-Duct, a tool facilitating analysis of the flow of solvent molecules in molecular dynamic simulations
+# Copyright (C) 2016  Tomasz Magdziarz <info@aquaduct.pl>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """
 This is a driver for Aqueduct.
 """
@@ -2011,7 +2028,9 @@ Valve driver version %s''' % (aqueduct_version_nice(), version_nice())
     parser.add_argument("-c", action="store", dest="config_file", required=False, help="Config file filename.")
     parser.add_argument("--max-frame", action="store", dest="max_frame", required=False, help="Limit number of frames.")
     parser.add_argument("--version", action="store_true", dest="print_version", required=False,
-                        help="Prints versions and exits..")
+                        help="Prints versions and exits.")
+    parser.add_argument("--license", action="store_true", dest="print_license", required=False,
+                        help="Prints short license info and exits.")
 
     args = parser.parse_args()
 
@@ -2049,6 +2068,31 @@ Valve driver version %s''' % (aqueduct_version_nice(), version_nice())
         print description
         print description_version
         exit(0)
+    # special case of license
+    if args.print_license:
+        valve_begin()
+        print "Licensed under GNU GPL v3. Full text of the license is distributed"
+        print "with installation package and is also available at"
+        print "https://www.gnu.org/licenses/gpl-3.0.txt"
+        print ""
+        print '''Aqua-Duct, a tool facilitating analysis of the flow of solvent molecules in molecular dynamic simulations
+Copyright (C) 2016  Tomasz Magdziarz <info@aquaduct.pl>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+        exit(0)
+
 
     ############################################################################
     # begin!
