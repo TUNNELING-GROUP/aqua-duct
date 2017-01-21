ee1e394f705bf0ae46eebf5fa50a77c9670626c1
Tomasz Magdziarz <tljm@wp.pl>
2017-01-21 11:39:08 +0100

    small error corrections

--- src/apps/valve.py
+++ src/apps/valve.py
@@ -2033,7 +2033,7 @@ if __name__ == "__main__":
         import argparse
 
         description_version = '''Aquaduct library version %s
-    Valve driver version %s''' % (aquaduct_version_nice(), version_nice())
+    Valve driver version %s''' % (aqueduct_version_nice(), version_nice())
         description = '''Valve, Aquaduct driver'''
 
         parser = argparse.ArgumentParser(description=description,
@@ -2121,9 +2121,9 @@ if __name__ == "__main__":
     '''
             exit(0)
 
-    # get global options
-    goptions = config.get_global_options()
-    # pbar_name = goptions.pbar
+        # get global options
+        goptions = config.get_global_options()
+        # pbar_name = goptions.pbar
 
         ############################################################################
         # begin!
