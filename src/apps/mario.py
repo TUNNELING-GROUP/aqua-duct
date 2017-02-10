509a9ddda439f62ac5062a3fc47353c3988247a6
Michał <stolarczyk.michal93@gmail.com>
2017-02-10 12:56:31 +0100

    Further work on mario.py

--- src/apps/mario.py
+++ src/apps/mario.py
@@ -24,33 +24,85 @@ if __name__ == "__main__":
         description = '''Mario, a tool for trajectories' properties visualization'''# TODO: przemyslec
         parser = argparse.ArgumentParser(description=description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         parser.add_argument("-i", action="store", dest="filename", required=True, help="Input file with paths to visualize.")
-        parser.add_argument("-n", action="store", dest="num", required=True, help="ID of molecule to visualize.")
-        parser.add_argument("-p", action="store", dest="plot_type", required=False, help="Type of plot(s) to be displayed. Separated by comma.")
-        #Dodac nieobowiazkowe parametry polyorder i window_length
+        parser.add_argument("-n", action="store", dest="path_number", required=True, help="ID of path to visualize.")
+        parser.add_argument("-p", action="store", dest="plot_type", required=True, help="Type of plot(s) to be displayed. Separated by comma. Plots available: velocity, acceleration", default=None)
+        parser.add_argument("-o", action="store", dest="poly_order", required=False, help="Order of polynomial to be used in Savitzky-Golay filter.", default=2)
+        parser.add_argument("-w", action="store", dest="window_size", required=False, help="Window length to be used in Savitzky-Golay filter.", default=51)
+
         #Kolory (midpoints)
-        #create help!
         #0. Create list^ of plots available, 2. Read the string with names from -p args, 2. Divide by comma (list* will be created) 3. Check elements of the list* for any misamtched with list^. 4. Think about automatization of plotting process
+
+        plots_avail = ['velocity', 'acceleration']
         args = parser.parse_args()
+
         # Argument checking section
         try:
-            args.num = int(args.num)
+            args.path_number = int(args.path_number)
         except ValueError:
-            logger.error("Oops!  That was no valid number. Integer required.")
+            logger.error("Oops!  That was not a valid number. Integer required.")
             raise ValueError("Oops!  That was no valid number. Integer required.")
 
+        try:
+            args.poly_order = int(args.poly_order)
+        except ValueError:
+            logger.error("Oops!  That was not a valid polynomial order. Integer required.")
+            raise ValueError("Oops!  That was no valid polynomial order. Integer required.")
+
+        try:
+            args.window_size = int(args.window_size)
+        except ValueError:
+            logger.error("Oops!  That was not a valid window size. Integer required.")
+            raise ValueError("Oops!  That was no valid window size. Integer required.")
+
         loaded_data = load_stage_dump(args.filename)
-        assert ((args.num >= 0) & (args.num <= len(loaded_data['spaths']))), "Path number should be in range 0 - " + str(len(loaded_data['spaths'])) #Check if path ID is accessible
+        assert ((args.path_number >= 0) & (args.path_number <= len(loaded_data['spaths']))), "Path number should be in range 0 - " + str(len(loaded_data['spaths'])) #Check if path ID is accessible
+        assert (len(loaded_data['spaths'][args.path_number].get_velocity_cont()) >= 3), "Path is too short to be visualized."
 
-        divided_paths = map(len, loaded_data['spaths'][args.num].paths)
-        velocity = loaded_data['spaths'][args.num].get_velocity_cont() #Getting velocity for each frame the path with ID = args.num was detected
-        SG_filtered = savgol_filter(x=velocity, window_length=75, polyorder=2)
-        plt.plot()
-        plt.plot(velocity)
-        plt.show()
+        plots_user = args.plot_type.split(',')
+        plots = list(set(plots_avail).intersection(plots_user))
+        assert (len(plots) > 0), "Oops!  Invalid name(s) of plots provided."
 
-        distance = loaded_data['spaths'][args.num].get_distance_cont()  #Getting distance travelled for each frame the path with ID = args.num was detected
-        plt.plot(distance)
+        divided_paths = map(len, loaded_data['spaths'][args.path_number].paths)
+        print divided_paths
+        indices_incoming = range(divided_paths[0])
+        indices_inside = range(divided_paths[0], divided_paths[1]+divided_paths[0])
+        indices_outgoing = range(divided_paths[1]+divided_paths[0], divided_paths[0]+divided_paths[1]+divided_paths[2])
 
 
-        plt.plot(distance,velocity)
-        plt.show()
+        if 'velocity' in plots:
+            velocity = loaded_data['spaths'][args.path_number].get_velocity_cont() #Getting velocity for each frame the path with ID = args.num was detected
+            distance = loaded_data['spaths'][args.path_number].get_distance_cont()  # Getting distance travelled for each frame the path with ID = args.num was detected
+            try:
+                SG_filtered = savgol_filter(x=velocity, window_length=args.window_size, polyorder=args.poly_order)
+            except IOError:
+                logger.error("Oops!  This path is too short to be visualized.")
+                raise ValueError("Oops!  This path is too short to be visualized.")
+
+            plt.plot(distance, velocity, 'k')
+            plt.plot(distance[indices_incoming], SG_filtered[indices_incoming], 'r')
+            plt.plot(distance[indices_inside], SG_filtered[indices_inside], 'g')
+            plt.plot(distance[indices_outgoing], SG_filtered[indices_outgoing], 'b')
+            plt.title("Velocity plot")
+            plt.xlabel("Distance")
+            plt.ylabel("Velocity")
+            plt.show()
+
+        if 'acceleration' in plots:
+            acceleration = loaded_data['spaths'][args.path_number].get_acceleration_cont()
+            distance = loaded_data['spaths'][args.path_number].get_distance_cont()  # Getting distance travelled for each frame the path with ID = args.num was detected
+            try:
+                SG_filtered = savgol_filter(x=acceleration, window_length=args.window_size, polyorder=args.poly_order)
+            except IOError:
+                logger.error("Oops!  This path is too short to be visualized.")
+                raise ValueError("Oops!  This path is too short to be visualized.")
+
+            plt.plot(distance, acceleration, 'k')
+            plt.plot(distance[indices_incoming], SG_filtered[indices_incoming], 'r')
+            plt.plot(distance[indices_inside], SG_filtered[indices_inside], 'g')
+            plt.plot(distance[indices_outgoing], SG_filtered[indices_outgoing], 'b')
+            plt.title("Acceleration plot")
+            plt.xlabel("Distance")
+            plt.ylabel("Velocity")
+            plt.show()
+
+
