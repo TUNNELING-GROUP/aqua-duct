corrected ETA calculation in simple progress bar, corrected number of expected interations in master path generation - correct pbar display

src/aquaduct/utils/clui.py
@@ -304,7 +304,7 @@ class SimpleProgressBar(object):
         if self.current == 0:
             return '?'
         if len(self.tictoclist)>2:
-            diff = np.mean(np.diff(self.tictoclist)) * self.current
+            diff = (np.mean(np.diff(self.tictoclist))/(self.maxval/100.)) * self.current
         else:
             diff = self.tcurrent - self.begin
         periteration = diff / self.current
@@ -340,7 +340,7 @@ class SimpleProgressBar(object):
         percent = self.percent()
         if int(percent) not in self.hundreds:
             self.hundreds.append(int(percent))
-            self.tictoclist.append(self.current)
+            self.tictoclist.append(self.tcurrent)
 
             while len(self.tictoclist) > 20:
                 self.tictoclist.pop(0)