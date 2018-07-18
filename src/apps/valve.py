4ffd8d4d37dd2888185badf1e77376f45dd75402
Tomasz Magdziarz <t.magdziarz@tunnelinggroup.pl>
2018-07-18 15:13:01 +0200

    new options in valve from v0.5

--- src/apps/valve.py
+++ src/apps/valve.py
@@ -85,17 +85,16 @@ if __name__ == "__main__":
                             help="Prints versions and exits.")
         parser.add_argument("--license", action="store_true", dest="print_license", required=False,
                             help="Prints short license info and exits.")
+        parser.add_argument("--netcdf", action="store_true", dest="use_netcdf", required=False,
+                            help="Use AQ NetCDF format as default.")
+        parser.add_argument("--netcdf4", action="store_true", dest="use_netcdf4", required=False,
+                            help="Use AQ NetCDF format as default using netCDF4 if available.")
+        parser.add_argument("--force-save", action="store_true", dest="force_save", required=False,
+                            help="Force saving results.")
+        parser.add_argument("--force-netcdf", action="store_true", dest="force_netcdf", required=False,
+                            help="Force saving results in AQ NetCDF format.")
 
         args = parser.parse_args()
-
-        ############################################################################
-        # cache dir!
-        GCS.cachedir = args.cachedir
-        GCS.cachemem = args.cachemem
-        load_cric()
-
-        from aquaduct.traj.sandwich import Reader, Window
-        from aquaduct.apps.valvecore import *
 
         ############################################################################
         # debug
@@ -115,6 +114,17 @@ if __name__ == "__main__":
         # finally, get valve logger
         logger = logging.getLogger(logger_name + '.valve')
         logger.info('Initialization of Valve logging done.')
+
+        ############################################################################
+        # cache dir & netcdf
+        GCS.cachedir = args.cachedir
+        GCS.cachemem = args.cachemem
+        GCS.netcdf = args.use_netcdf or args.use_netcdf4
+        GCS.nc4 = args.use_netcdf4
+        load_cric()
+
+        from aquaduct.traj.sandwich import Reader, Window
+        from aquaduct.apps.valvecore import *
 
         ############################################################################
         # single precision storage
@@ -220,28 +230,30 @@ if __name__ == "__main__":
         if args.sandwich:
             clui.message("Sandwich mode with %d layers." % len(Reader.trajectory))
 
-        ## TODO: Is it reported correctly?
-        # clui.message("Using %d of %d available frames." % (max_frame + 1, reader.max_frame + 1))
+        # force_save
+        force_save = args.force_save
+        if force_save and args.force_netcdf:
+            force_save = 'nc'
 
         # container for collecting whether particular stage was executed
         run_status = {}
 
         # STAGE I
-        result1 = valve_exec_stage(0, config, stage_I_run,
+        result1 = valve_exec_stage(0, config, stage_I_run, force_save=force_save,
                                    run_status=run_status)
 
         # STAGE II
-        result2 = valve_exec_stage(1, config, stage_II_run,
+        result2 = valve_exec_stage(1, config, stage_II_run, force_save=force_save,
                                    run_status=run_status,
                                    **result1)
 
         # STAGE III
-        result3 = valve_exec_stage(2, config, stage_III_run,
+        result3 = valve_exec_stage(2, config, stage_III_run, force_save=force_save,
                                    run_status=run_status,
                                    **result2)
 
         # STAGE IV
-        result4 = valve_exec_stage(3, config, stage_IV_run,
+        result4 = valve_exec_stage(3, config, stage_IV_run, force_save=force_save,
                                    run_status=run_status,
                                    **result3)
 
