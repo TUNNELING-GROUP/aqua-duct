TUNNELING-GROUP/aqua-duct

commit 00d95c5cb2c05adea670bb87f645b371acb24780

aquaduct/traj/sandwich.py:882: FutureWarning: arrays to stack must be passed as a "sequence" type such as list or tuple. Support for non-sequence iterables such as generators is deprecated as of NumPy 1.16 and will raise an error in the future. return np.vstack(get_coords_from_cache())
Hope to be fixed.

src/aquaduct/traj/sandwich.py
@@ -874,6 +874,7 @@ def coords_range(srange, number, rid):
     '''
 
     # call get_ranges and stack array, do it in comprehension? nested function?
+    @arrayify(shape=(None, 3))
     def get_coords_from_cache():
         for sr, xr in CRIC.get_frc(number, rid).get_ranges(srange):
             logger.debug("CRIC partial request %d:%d %s", number, rid, str(sr))

commit 91260532470e1f8044d9288994d777f492dfcb0d

fixing coords_range - problem was introduced by 00d95c5cb2c05adea670bb87f645b371acb24780

src/aquaduct/traj/sandwich.py
@@ -857,6 +857,7 @@ def coords_range_core(srange, number, rid):
     return coords_range_core_inner(srange, number, rid)
 
 
+@arrayify(shape=(None, 3))
 def coords_range(srange, number, rid):
     # srange is SmartRangeIncrement, it cannot be anything else
     # wrapper to limit number of calls to coords_range_core
@@ -872,13 +873,12 @@ def coords_range(srange, number, rid):
     '''
 
     # call get_ranges and stack array, do it in comprehension? nested function?
-    @arrayify(shape=(None, 3))
     def get_coords_from_cache():
         for sr, xr in CRIC.get_frc(number, rid).get_ranges(srange):
             logger.debug("CRIC partial request %d:%d %s", number, rid, str(sr))
             yield coords_range_core(sr, number, rid)[xr, :]
 
-    return np.vstack(get_coords_from_cache())
+    return np.vstack(tuple(get_coords_from_cache()))
 
 
 ################################################################################