918308762a0f1563561680566b48ffbd7c77b87c
Michał Banas <b4naser@gmail.com>
2019-01-23 11:08:12 +0100

    fixed scrolling with mouse wheel

--- src/aquaduct/apps/valveconfig/utils.py
+++ src/aquaduct/apps/valveconfig/utils.py
@@ -706,35 +706,51 @@ class VerticalScrolledFrame(tk.Frame):
         # create a canvas object and a vertical scrollbar for scrolling it
         vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
         vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
-        canvas = tk.Canvas(self, bd=0, highlightthickness=0,
-                           yscrollcommand=vscrollbar.set)
-        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
-        vscrollbar.config(command=canvas.yview)
+        self.canvas = tk.Canvas(self, bd=0, highlightthickness=0,
+                                yscrollcommand=vscrollbar.set)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
+        vscrollbar.config(command=self.canvas.yview)
 
         # reset the view
-        canvas.xview_moveto(0)
-        canvas.yview_moveto(0)
+        self.canvas.xview_moveto(0)
+        self.canvas.yview_moveto(0)
 
         # create a frame inside the canvas which will be scrolled with it
-        self.interior = interior = tk.Frame(canvas)
-        interior_id = canvas.create_window(0, 0, window=interior,
-                                           anchor="nw")
+        self.interior = interior = tk.Frame(self.canvas)
+        interior_id = self.canvas.create_window(0, 0, window=interior, anchor="nw")
 
         # track changes to the canvas and frame width and sync them,
         # also updating the scrollbar
-        def _configure_interior(event):
+        def _configure_interior(e):
             # update the scrollbars to match the size of the inner frame
             size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-            canvas.config(scrollregion="0 0 %s %s" % size)
-            if interior.winfo_reqwidth() != canvas.winfo_width():
+            self.canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != self.canvas.winfo_width():
                 # update the canvas's width to fit the inner frame
-                canvas.config(width=interior.winfo_reqwidth())
+                self.canvas.config(width=interior.winfo_reqwidth())
 
         interior.bind('<Configure>', _configure_interior)
 
-        def _configure_canvas(event):
-            if interior.winfo_reqwidth() != canvas.winfo_width():
+        def _configure_canvas(e):
+            if interior.winfo_reqwidth() != self.canvas.winfo_width():
                 # update the inner frame's width to fill the canvas
-                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+                self.canvas.itemconfigure(interior_id, width=self.canvas.winfo_width())
 
-        canvas.bind('<Configure>', _configure_canvas)
+        self.canvas.bind('<Configure>', _configure_canvas)
+
+        self.canvas.bind('<Enter>', self._bind_mousewheel)
+        self.canvas.bind('<Leave>', self._unbind_mousewheel)
+
+    def _bind_mousewheel(self, e):
+        def _mousewheel_handler(e, canvas):
+            if e.num == 5:
+                canvas.yview_scroll(1, "units")
+            if e.num == 4:
+                canvas.yview_scroll(-1, "units")
+
+        self.canvas.bind_all("<MouseWheel>", lambda e: _mousewheel_handler(e, self.canvas))
+        self.canvas.bind_all("<Button-4>", lambda e: _mousewheel_handler(e, self.canvas))
+        self.canvas.bind_all("<Button-5>", lambda e: _mousewheel_handler(e, self.canvas))
+
+    def _unbind_mousewheel(self, e):
+        self.canvas.unbind("<MouseWheel>")
