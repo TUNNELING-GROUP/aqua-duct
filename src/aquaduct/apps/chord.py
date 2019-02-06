b526766db1e925f30ec1948d9b6c2b3737a656e2
Michał Banas <b4naser@gmail.com>
2019-02-06 11:46:14 +0100

    links scaling and direction arrows

--- src/aquaduct/apps/chord.py
+++ src/aquaduct/apps/chord.py
@@ -3,13 +3,16 @@ import matplotlib.path as mpath
 import matplotlib.pyplot as plt
 import numpy as np
 
+def hex2rgb(color):
+    # https://stackoverflow.com/questions/29643352/converting-hex-to-rgb-value-in-python
+    return tuple(int(color[i:i+2], 16) for i in (0, 2 ,4))
 
 def polar2point(angle, r):
     rad = np.deg2rad
     return np.array([r * np.sin(rad(angle)), r * np.cos(rad(angle))])
 
 
-def generate_arc(r, sa, ea, max_angle=45, reversed_=False):
+def generate_arc(r, sa, ea, max_angle=5, reversed_=False):
     """
     Generate arc vertrices with control points for quadratic Bezier curve.
 
@@ -76,7 +79,7 @@ class Node(mpatches.PathPatch):
         codes.extend([mpath.Path.CURVE3] * (len(outer_arc) - 1))
 
         # Inner arc
-        inner_arc = generate_arc(r - 1, sa, ea, reversed_=True)
+        inner_arc = generate_arc(0.9*r, sa, ea, reversed_=True)
         vertices.extend(inner_arc)
 
         codes.append(mpath.Path.LINETO)
@@ -91,6 +94,10 @@ class Node(mpatches.PathPatch):
         super(Node, self).__init__(path, facecolor=color, linewidth=0)
 
     def reserve_arc(self, angle):
+        # Only if scaling is an option
+        # if self._link_angle + angle - self.sa > self.ea - self.sa:
+        #     raise RuntimeError("Too much data for node. Use scaling option.")
+
         self._link_angle += angle
 
     def get_arc_offset(self):
@@ -150,17 +157,23 @@ class Link(mpatches.PathPatch):
 
 
 class Arrow(mpatches.PathPatch):
-    def __init__(self, r, sa, ea, color):
+    def __init__(self, r, sa, ea, color, max_angle=45):
+        # When angle is > 60 arrow isnt arrow
         vertices = []
         codes = []
 
-        arc = generate_arc(r, sa, ea)
+        c = sa + (ea - sa)/2
+
+        if ea - sa < max_angle:
+            max_angle = ea - sa
+
+        arc = generate_arc(r, c - max_angle/2, c + max_angle/2)
         vertices.extend(arc)
 
         codes.append(mpath.Path.MOVETO)
         codes.extend([mpath.Path.CURVE3] * (len(arc) - 1))
 
-        vertices.append(polar2point(sa + (ea - sa) / 2, r - 2))
+        vertices.append(polar2point(sa + (ea - sa) / 2, 0.9*r - 2))
         codes.append(mpath.Path.LINETO)
 
         vertices.append((0, 0))
@@ -187,7 +200,7 @@ class Chord(object):
             ea = sa + size * 360. / sizes_sum
 
             angle = sa + (ea - sa) / 2
-            pos = polar2point(angle, r + 0.5)
+            pos = polar2point(angle, 1.05*r)
             ax.text(pos[0], pos[1],
                     labels[i],
                     verticalalignment="center",
@@ -202,31 +215,53 @@ class Chord(object):
 
             sa = ea
 
+        # Scales
+        links_sum = [0.]*len(nodes_sizes)
+        for link in links:
+            links_sum[link["source"]] += link["value"]
+            links_sum[link["dest"]] += link["value"]
+
+        scales = []
+        for i, (size, link_sum) in enumerate(zip(nodes_sizes, links_sum)):
+            if size < link_sum:
+                scales.append(0.9999 * size / link_sum)  # Scale is reduced to 99% due to float precision
+            else:
+                scales.append(1.)
+
         # Links
         for link in links:
             source_node = self.nodes[link["source"]]
             dest_node = self.nodes[link["dest"]]
             value = link["value"]
             sa0 = source_node.get_arc_offset()
-            sa1 = sa0 + value * 360. / sizes_sum
+            slink_arc = scales[link["source"]] * value * 360. / sizes_sum
+            sa1 = sa0 + slink_arc
 
-            source_node.reserve_arc(value * 360. / sizes_sum)
+            source_node.reserve_arc(slink_arc)
 
             ea0 = dest_node.get_arc_offset()
-            ea1 = ea0 + value * 360. / sizes_sum
+            elink_arc = scales[link["dest"]] * value * 360. / sizes_sum
+            ea1 = ea0 + elink_arc
 
-            dest_node.reserve_arc(value * 360. / sizes_sum)
+            dest_node.reserve_arc(elink_arc)
 
-            l = Link(r - 1, sa0, sa1, ea0, ea1, source_node.color)
-            a = Arrow(r - 1, sa0, sa1, source_node.color)
+            l = Link(0.92*r, sa0, sa1, ea0, ea1, source_node.color)
+            a = Arrow(0.92*r, sa0, sa1, source_node.color)
 
-            # # Arrow text
-            # ax.text(pos[0], pos[1],
-            #         labels[i],
-            #         verticalalignment="center",
-            #         horizontalalignment="center",
-            #         fontsize=8,
-            #         rotation=360-angle)
+            # Invert color
+            rgbcolor = hex2rgb(source_node.color.lstrip("#")) if isinstance(source_node.color, str) else source_node.color
+            inverted_color = [(-c + 255) / 255. for c in rgbcolor]
+
+            # Arrow text
+            angle = sa0 + (sa1 - sa0)/2
+            pos = polar2point(sa0 + (sa1 - sa0)/2, 0.9*r)
+            ax.text(pos[0], pos[1],
+                    link["value"],
+                    verticalalignment="center",
+                    horizontalalignment="center",
+                    fontsize=6,
+                    rotation=180 - angle,
+                    color=inverted_color)
 
             ax.add_patch(l)
             ax.add_patch(a)
@@ -235,19 +270,20 @@ class Chord(object):
 if __name__ == "__main__":
     fig, ax = plt.subplots(subplot_kw={"aspect": 1})
     ax.set_axis_off()
-    ax.set_xlim(-11, 11)
-    ax.set_ylim(-11, 11)
+    ax.set_xlim(-110, 110)
+    ax.set_ylim(-110, 110)
 
     labels = ["Data #1", "Data #2", "Data #3", "Data #4"]
 
-    sizes = [120, 120, 120, 70]
+    sizes = [5000, 6000, 7000]
 
-    links = [dict(source=0, dest=1, value=10),
-             dict(source=2, dest=2, value=30),
-             dict(source=1, dest=2, value=10),
-             dict(source=2, dest=3, value=20),
-             dict(source=2, dest=0, value=10)]
+    links = [dict(source=1, dest=0, value=2000),
+             dict(source=1, dest=0, value=2000),
+             dict(source=2, dest=0, value=4600),
+             dict(source=0, dest=0, value=1000)]
 
-    Chord(ax, 10, sizes, links, labels)
+    Chord(ax, 100, sizes, links, labels)
+
+    fig.savefig("chord.png", format="png", dpi=2**7)
 
     plt.show()
