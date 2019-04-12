5887d0412a15553d52d2de4c48aefc6a3e39b987
Michał Banas <b4naser@gmail.com>
2019-04-12 11:01:33 +0200

    using energy reference for scaling hotspots size

--- src/apps/hs_resize.py
+++ src/apps/hs_resize.py
@@ -18,66 +18,52 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from pymol import cmd, stored
+import json
+import gzip
 import random
 import string
 
 
 def _sele_exists(sele):
-	return sele in cmd.get_names("all")
+    return sele in cmd.get_names("all")
 
 
-def _random_string(n = 10):
-	return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
+def _random_string(n=10):
+    return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(n))
 
 
-def hs_resize(selection):
-	"""
-Changes size of hotspots depending on normalized partial_charge values.
+def hs_resize(meta_file, selection):
+    """
+    Changes size of hotspots depending on normalized partial_charge values.
 
-USAGE:
-hs_resize selection
-	"""
-	if not _sele_exists(selection):
-		raise RuntimeError("Selection \"{}\" does not exists.".format(selection))
+    USAGE:
+    hs_resize /path/to/pond_meta.json hotspot_selection
+    """
+    if not _sele_exists(selection):
+        raise RuntimeError("Selection \"{}\" does not exists.".format(selection))
 
-	# Find free sele name
-	temp_sele = _random_string()
-	while _sele_exists(temp_sele):
-		temp_sele = _random_string()
+    # Find free sele name
+    temp_sele = _random_string()
+    while _sele_exists(temp_sele):
+        temp_sele = _random_string()
 
-	states = cmd.count_states(selection=selection)
+    states = cmd.count_states(selection=selection)
 
-	# Find min/max
-	min_ = 0
-	max_ = 0
-	for state in range(1, states+1):
-		stored.info = []
-		cmd.iterate_state(state, "all", "stored.info.append((partial_charge))")
+    with gzip.open("/home/b4naser/Desktop/10ns/qwe/pond_meta.json") as f:
+        ref = float(json.load(f)["reference_density_correction"])
 
-		state_min = min(stored.info)
-		state_max = max(stored.info)
+    for state in range(1, states+1):
+        stored.info = []
+        cmd.iterate_state(state, selection, "stored.info.append((ID, partial_charge))")
 
-		if state_min < min_:
-			min_ = state_min
+        for id_, partial_charge in stored.info:
+            size = partial_charge/ref * 1.
 
-		if state_max > max_:
-			max_ = state_max
+            cmd.select(temp_sele, "id {}".format(id_), state=state)
+            cmd.set("sphere_scale", value=size, selection=temp_sele)
+            cmd.alter(temp_sele, "b={}".format(partial_charge))
 
-	for state in range(1, states+1):
-		stored.info = []
-		cmd.iterate_state(state, "all", "stored.info.append((partial_charge))")
-
-		min_ = min(partial_charge)
-		max_ = max(partial_charge)
-
-		for id_, partial_charge in stored.info:
-			size = (partial_charge - min_) * (1. - 0.1) / (max_ - min_) + 0.1
-
-			cmd.select(temp_sele, "id {}".format(id_), state=state)
-			cmd.set("sphere_scale", value=size, selection=temp_sele)
-			cmd.alter(temp_sele, "b={}".format(partial_charge))
-
-	cmd.delete(temp_sele)
+    cmd.delete(temp_sele)
 
 
 cmd.extend("hs_resize", hs_resize)
