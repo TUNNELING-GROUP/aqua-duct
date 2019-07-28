60a2202c95a4e3f9f9785aab9036b3a2a2c9077c
Tomasz Magdziarz <tljm@wp.pl>
2019-07-28 11:08:12 +0200

    correcting docs helper script

--- docs/cp_html_.sh
+++ docs/cp_html_.sh
@@ -1,23 +1,23 @@
-rm -rf $(find ../../TUNNELING-GROUP.github.io/aqua-duct/ | grep -v -F v0.)
+rm -rf $(find ../../TUNNELING-GROUP.github.io/aqua-duct/* | grep -v -F v0.)
 cp -R build/html/* ../../TUNNELING-GROUP.github.io/aqua-duct/
 
 CWD=`pwd`
 
 cd ../../TUNNELING-GROUP.github.io/aqua-duct/
 
-mv static static
-mv images images
-mv modules modules
-mv sources sources
+mv _static static
+mv _images images
+mv _modules modules
+mv _sources sources
 
-sed -i 's/static/static/g' `grep -Rl 'static' *`
-sed -i 's/images/images/g' `grep -Rl 'images' *`
-sed -i 's/modules/modules/g' `grep -Rl 'modules' *`
-sed -i 's/sources/sources/g' `grep -Rl 'sources' *`
+sed -i 's/_static/static/g' `grep -Rl 'static' *`
+sed -i 's/_images/images/g' `grep -Rl 'images' *`
+sed -i 's/_modules/modules/g' `grep -Rl 'modules' *`
+sed -i 's/_sources/sources/g' `grep -Rl 'sources' *`
 
-git add .
-git commit --message "docs update $(date)"
-git push
+#git add .
+#git commit --message "docs update $(date)"
+#git push
 
 cd $CWD
 
