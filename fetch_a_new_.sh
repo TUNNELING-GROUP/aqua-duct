b9c38b1ae3ba4c60776fe4c2fbe60ce2df690ec0
Tomasz Magdziarz <tljm@wp.pl>
2025-03-13 16:04:38 +0100

    Enhance fetch script to support dynamic branch and tag imports, with optional push to new remote

--- fetch_a_new_.sh
+++ fetch_a_new_.sh
@@ -1,8 +1,20 @@
+#!/bin/sh
 
+# Git URLs
+SOURCE_URL="git@gitlab.com:tljm/aqua-duct.git"
+TARGET_URL="git@github.com:TUNNELING-GROUP/aqua-duct.git"
+
+# Check if --push option is provided
+PUSH=false
+if [ "$1" = "--push" ]; then
+    PUSH=true
+else
+    echo "Warning: --push option not used. Changes will not be pushed to the remote repository."
+fi
 
 # target repo
 REPO="test"
-# init clean target reop
+# init clean target repo
 rm -rf $REPO $REPO.git
 mkdir $REPO.git
 cd $REPO.git
@@ -17,19 +29,21 @@ cd ..
 # source repo
 SOURCE="source"
 rm -rf $SOURCE
-git clone https://gitlab.com/tljm/aqua-duct.git $SOURCE
+git clone $SOURCE_URL $SOURCE
 
 cd $REPO
 
 # GitLab updates ###############################################################
 
 # import branches
+cd ../$SOURCE
+BRANCHES=$(git branch -r | grep -v '\->' | sed 's/ *origin\///')
+echo "Branches to be imported: $BRANCHES"
+cd ../$REPO
 
-for BRANCH in v0.2 v0.3 v0.4 v0.5 v0.5-no_exchange v1.0
-do
-	git fetch https://gitlab.com/tljm/aqua-duct.git $BRANCH
-    if [ $? -eq 0 ]
-    then
+for BRANCH in $BRANCHES; do
+    git fetch $SOURCE_URL $BRANCH
+    if [ $? -eq 0 ]; then
         git checkout FETCH_HEAD
         git checkout -b $BRANCH
         git push --set-upstream origin $BRANCH
@@ -38,31 +52,36 @@ done
 
 # import tags
 cd ../$SOURCE
-TAGS=`git tag`
+TAGS=$(git tag)
+echo "Tags to be imported: $TAGS"
 cd ../$REPO
 
-for T in $TAGS
-do
-	git fetch https://gitlab.com/tljm/aqua-duct.git $T
-	if [ $? -eq 0 ]
-	then
-		git checkout FETCH_HEAD
-		git tag $T
-		git push origin --tags
-	fi
+for T in $TAGS; do
+    git fetch $SOURCE_URL $T
+    if [ $? -eq 0 ]; then
+        git checkout FETCH_HEAD
+        git tag $T
+        git push origin --tags
+    fi
 done
 
-#exit 0
+# Ask user if they want to proceed with pushing to new remote
+read -p "Do you want to proceed with pushing to new remote? (y/n): " proceed_remote
+if [ "$proceed_remote" != "y" ]; then
+    exit 0
+fi
 
-git remote set-url origin https://github.com/TUNNELING-GROUP/aqua-duct.git
+git remote set-url origin $TARGET_URL
 
 # push to new remote
-for BRANCH in v0.2 v0.3 v0.4 v0.5 v0.5-no_exchange v1.0
-do
-    git checkout $BRANCH
-    git push
-done
-git push --tags
+if [ "$PUSH" = true ]; then
+    for BRANCH in $BRANCHES; do
+        git checkout $BRANCH
+        git push
+    done
+    git push --tags
+else
+    echo "Warning: Skipping push to new remote due to missing --push option."
+fi
 
 cd ..
-
